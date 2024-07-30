b3a08e417ae3f2521329f6178841c68d36ee20ee
Paul Yu <paul.d.yu@gmail.com>
2024-07-29 21:48:10 -0700

    remove load test resource
    Signed-off-by: Paul Yu <paul.d.yu@gmail.com>

--- 2024-07-16-argo-rollouts-with-istio/loadtest.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_load_test" "example" {
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  name                = "lt-${local.random_name}"
-}
